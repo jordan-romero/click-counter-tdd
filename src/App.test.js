ad01ba240df2098e2844f4b4d926767089685bc4
jordles113 <jordan.t.romero@gmail.com>
2021-01-01 17:41:05 -0600

    boiler plates tests for click counter

--- src/App.test.js
+++ src/App.test.js
@@ -7,4 +7,22 @@ import App from './App';
 
 Enzyme.configure({ adapter: new EnzymeAdapter() })
 
+test('renders without error', () => {
 
+})
+
+test('renders button', () => {
+
+})
+
+test('renders counter display', () => {
+
+})
+
+test('counter starts at 0', () => {
+
+})
+
+test('clicking on button increments counter', () => {
+
+})
