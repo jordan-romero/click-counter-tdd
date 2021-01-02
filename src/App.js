0d34d75d1f774c0ea4ea2361f43a0c013cf4cdef
jordles113 <jordan.t.romero@gmail.com>
2021-01-02 15:42:20 -0600

    adds click functionality

--- src/App.js
+++ src/App.js
@@ -9,7 +9,9 @@ function App() {
     <div data-test='component-app'>
       <h1 data-test='counter-display'>
       The counter is currently&nbsp;<span data-test='count'>{count}</span></h1>
-      <button data-test='increment-button'>Increment Counter</button>
+      <button 
+      data-test='increment-button'
+      onClick={() => setCount(count + 1)}>Increment Counter</button>
     </div>
   );
 }
