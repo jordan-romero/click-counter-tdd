d0cee37b13588767864da6d471c8a18e0db39490
jordles113 <jordan.t.romero@gmail.com>
2021-01-02 15:37:25 -0600

    adds state, writes test to determine if counter starts at 0

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
 
 function App() {
+
+  const [count, setCount] = React.useState(0)
+
   return (
     <div data-test='component-app'>
-      <h1 data-test='counter-display'>The counter is currently</h1>
+      <h1 data-test='counter-display'>
+      The counter is currently&nbsp;<span data-test='count'>{count}</span></h1>
       <button data-test='increment-button'>Increment Counter</button>
     </div>
   );
--- src/App.test.js
+++ src/App.test.js
@@ -30,7 +30,9 @@ test('renders counter display', () => {
 })
 
 test('counter starts at 0', () => {
-
+  const wrapper = setup()
+  const count = findByTestAttr(wrapper, 'count').text()
+  expect(count).toBe('0')
 })
 
 test('clicking on button increments counter', () => {
